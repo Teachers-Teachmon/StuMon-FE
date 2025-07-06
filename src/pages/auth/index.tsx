056a2f1cbb22ffc8b9a3aefbd79a469aa197aab5
zuu3 <ojh27467242@gmail.com>
2025-07-06 12:28:17 +0900

    refactor :: 엑세스 토큰 설정 후 이니셜라이즈

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "../../components/ui/loading";
+import useAuth from "../../store/auth";
 
 export default function Auth() {
   const navigate = useNavigate();
+  const { initializeFromToken } = useAuth();
   useEffect(() => {
     const cookies = document.cookie.split(";").map((c) => c.trim());
     const accessTokenCookie = cookies.find((c) =>
@@ -13,11 +15,12 @@ export default function Auth() {
       const accessToken = accessTokenCookie.split("=")[1];
       if (accessToken) {
         localStorage.setItem("AT", accessToken);
+        initializeFromToken();
         navigate("/main");
         return;
       }
     }
-    else{
+    else {
       alert("로그인에 실패했습니다.");
       navigate("/");
     }
